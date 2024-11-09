c2b3ff9292825006fb3b98a7e41ee36ff6c26521
JacquelineCuenca <jacuenca@espol.edu.ec>
2024-11-09 13:02:57 -0500

    Update ManejoArchivos.java

--- POO4_1P_CAMUENDO_CUENCA_QUIMI/src/ManejoArchivos.java
+++ POO4_1P_CAMUENDO_CUENCA_QUIMI/src/ManejoArchivos.java
@@ -1,5 +1,13 @@
 package POO4_1P_CAMUENDO_CUENCA_QUIMI.src;
 
+import java.util.ArrayList;
+
 public class ManejoArchivos {
-    
+    public ArrayList<String> LeerFichero(String nombreArchivo) {
+        ArrayList<String> l = new ArrayList<>();
+        return l;
+    }
+
+    public void EcribirArchivo(String nombreArchivo, String linea) {
+    }
 }
