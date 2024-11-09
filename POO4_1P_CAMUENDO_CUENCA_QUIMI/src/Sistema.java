a177a4849481702bac60f05906575c842cd134c5
JacquelineCuenca <jacuenca@espol.edu.ec>
2024-11-09 13:19:42 -0500

    Update Sistema.java

--- POO4_1P_CAMUENDO_CUENCA_QUIMI/src/Sistema.java
+++ POO4_1P_CAMUENDO_CUENCA_QUIMI/src/Sistema.java
@@ -3,8 +3,8 @@ package POO4_1P_CAMUENDO_CUENCA_QUIMI.src;
 import java.util.ArrayList;
 
 public class Sistema {
+    public ArrayList<Reserva> reservas;
     public static void main(String[] args) {
         
-
     }
 }
