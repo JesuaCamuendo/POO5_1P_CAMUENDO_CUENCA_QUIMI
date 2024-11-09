f37d897a4f4fc01b73694a3bf2fd3030c511bfd1
Valeria Quimi <vdquimi@espol.edu.ec>
2024-11-09 13:17:07 -0500

    Proyecto

--- POO4_1P_CAMUENDO_CUENCA_QUIMI/src/Sistema.java
+++ POO4_1P_CAMUENDO_CUENCA_QUIMI/src/Sistema.java
@@ -1,7 +1,13 @@
 package POO4_1P_CAMUENDO_CUENCA_QUIMI.src;
 
 public class Sistema {
+    public ArrayList<Usuario> usuarios;
+
     public static void main(String[] args) {
+
+    }
+
+    public void iniciarSeccion(){
         
     }
 }
