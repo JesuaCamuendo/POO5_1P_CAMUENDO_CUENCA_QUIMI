0dbbe51942bddbdce30e18c746c52a13f8e5809d
JesuaCamuendo <jecadiaz@espol.edu.ec>
2024-11-09 04:26:15 -0500

    Create Administrador.java

---
+++ Administrador.java
@@ -0,0 +1,17 @@
+public class Administrador {
+    private String cargo;
+
+    Administrador(String cargo){
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void gestionarReserva(){
+    }
+}
